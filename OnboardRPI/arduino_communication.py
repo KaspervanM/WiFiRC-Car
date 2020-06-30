b4df7b6d1d2f91c2d39247e01dd6bc33d2988ba2
Restyled.io <commits@restyled.io>
2020-06-30 14:05:43 +0000

    Restyled by black

--- OnboardRPI/arduino_communication.py
+++ OnboardRPI/arduino_communication.py
@@ -2,19 +2,24 @@ from serial import Serial
 from time import sleep
 from os import system
 
-ser = Serial('/dev/ttyUSB0', 9600)
-ser.write('p')
+ser = Serial("/dev/ttyUSB0", 9600)
+ser.write("p")
 while True:
     read_serial = ser.readline()
-    if (read_serial[:9] == "__RPI__: "):
+    if read_serial[:9] == "__RPI__: ":
         msg = read_serial[9:]
         ssid = msg.split()[0]
         passwd = msg.split()[1]
 
-        system('sudo sed -i \'/network/,$d\' /etc/wpa_supplicant/wpa_supplicant.conf')
-        system('sudo wpa_passphrase ' + ssid + ' ' + passwd +
-               ' >> /etc/wpa_supplicant/wpa_supplicant.conf')
-        system('sudo wpa_cli -i wlan0 reconfigure')
-        system('sudo sed -i \'/#psk/d\' /etc/wpa_supplicant/wpa_supplicant.conf')
+        system("sudo sed -i '/network/,$d' /etc/wpa_supplicant/wpa_supplicant.conf")
+        system(
+            "sudo wpa_passphrase "
+            + ssid
+            + " "
+            + passwd
+            + " >> /etc/wpa_supplicant/wpa_supplicant.conf"
+        )
+        system("sudo wpa_cli -i wlan0 reconfigure")
+        system("sudo sed -i '/#psk/d' /etc/wpa_supplicant/wpa_supplicant.conf")
         exit()
-    sleep(100/1000)
+    sleep(100 / 1000)
