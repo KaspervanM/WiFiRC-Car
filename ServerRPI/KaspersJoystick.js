facd117c51ebef813d88354563d30df3ff2cd031
Kasper van Maasdam <kaspervanm@gmail.com>
2020-07-03 12:43:33 +0200

    Update KaspersJoystick.js

--- ServerRPI/KaspersJoystick.js
+++ ServerRPI/KaspersJoystick.js
@@ -19,52 +19,51 @@ $(document).ready(setTimeout(function() {
 
 	// Make the DIV element draggable:
 	dragElement(joystick);
+}, '2000'));
 
-	function dragElement(elmnt) {
-	  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-	  elmnt.onmousedown = dragMouseDown;
+function dragElement(elmnt) {
+	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	elmnt.onpointerdown = dragMouseDown;
 
-	  function dragMouseDown(e) {
-	    e = e || window.event;
-	    e.preventDefault();
-	    // get the mouse cursor position at startup:
-	    pos3 = e.clientX;
-	    pos4 = e.clientY;
-	    document.onmouseup = closeDragElement;
-	    // call a function whenever the cursor moves:
-	    document.onmousemove = elementDrag;
-	  }
-
-	  function elementDrag(e) {
-	    e = e || window.event;
-	    e.preventDefault();
-	    // calculate the new cursor position:
-	    pos1 = pos3 - e.clientX;
-	    pos2 = pos4 - e.clientY;
-	    pos3 = e.clientX;
-	    pos4 = e.clientY;
-	    // set the element's new position:
-	    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-	    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-
-			// check if it doesnt leave container
-			if (Math.hypot(getPos().x, getPos().y) > joystick_box.clientHeight/2){
-				elmnt.style.top = (elmnt.offsetTop + pos2) + "px";
-		    elmnt.style.left = (elmnt.offsetLeft + pos1) + "px";
-			}
-	  }
-
-	  function closeDragElement() {
-	    // stop moving when mouse button is released:
-	    document.onmouseup = null;
-	    document.onmousemove = null;
-
-	    elmnt.style.top = (getPos(1).y - elmnt.clientHeight/2) + "px";
-	    elmnt.style.left = (getPos(1).x - elmnt.clientWidth/2) + "px";
-	  }
+	function dragMouseDown(e) {
+		e = e || window.event;
+		e.preventDefault();
+		// get the mouse cursor position at startup:
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		document.onpointerup = closeDragElement;
+		// call a function whenever the cursor moves:
+		document.onpointermove = elementDrag;
 	}
 
-}, '1000'));
+	function elementDrag(e) {
+		e = e || window.event;
+		e.preventDefault();
+		// calculate the new cursor position:
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		// set the element's new position:
+		elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+		elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+		// check if it doesnt leave container
+		if (Math.hypot(getPos().x, getPos().y) > joystick_box.clientHeight/2){
+			elmnt.style.top = (elmnt.offsetTop + pos2) + "px";
+			elmnt.style.left = (elmnt.offsetLeft + pos1) + "px";
+		}
+	}
+
+	function closeDragElement() {
+		// stop moving when mouse button is released:
+		document.onpointerup = null;
+		document.onpointermove = null;
+
+		elmnt.style.top = (getPos(1).y - elmnt.clientHeight/2) + "px";
+		elmnt.style.left = (getPos(1).x - elmnt.clientWidth/2) + "px";
+	}
+}
 
 function getPos(parentonly = -1){
 	var joystick_box_pos = joystick_box.getBoundingClientRect();
