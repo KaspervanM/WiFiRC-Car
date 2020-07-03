a3ddcb5efdbead595577f190d76aefb0ac11eb4f
Kasper van Maasdam <kaspervanm@gmail.com>
2020-07-03 12:11:21 +0200

    Update control.js

--- ServerRPI/control.js
+++ ServerRPI/control.js
@@ -14,12 +14,12 @@ function updateSlider(slideAmount) {
 
 //Handle key presses
 function keyDownHandler(event) {
-	pressed = event.keyCode;
+	var pressed = event.keyCode;
 	if (pressed in KeyboardHelper && !keys.includes(pressed)) {
-		l = 0;
-		r = 0;
+		var l = 0;
+		var r = 0;
 		keys.map(function (num){if (KeyboardHelper[num].includes("l")) {l++;} else {r++;}});
-		if ((KeyboardHelper[pressed].includes("l") && l == 0) || (KeyboardHelper[pressed].includes("r") && r == 0)){
+		if ((KeyboardHelper[pressed].includes("l") && l === 0) || (KeyboardHelper[pressed].includes("r") && r === 0)){
 			keys.push(pressed);
 			combine();
 		}
@@ -28,7 +28,7 @@ function keyDownHandler(event) {
 
 //Handle key releases
 function keyUpHandler(event) {
-	index = keys.indexOf(event.keyCode);
+	var index = keys.indexOf(event.keyCode);
 	if (index > -1) {
 		keys.splice(index, 1);
 		combine();
@@ -38,11 +38,11 @@ function keyUpHandler(event) {
 //Send motor instructions
 function sendKey(key) {
 	console.log(key);
-	$.get("saver.php", {key:key});//,function success(e){/*$(".controls").append(key).append("  -  ");*/});
+	$.get("saver.php", {key:key});
 }
 //Send keys that where used
 function combine() {
-	combined = keys.map(function (num){if (num in KeyboardHelper) return (KeyboardHelper[num]);}).join('');
+	var combined = keys.map(function (num){if (num in KeyboardHelper) return (KeyboardHelper[num]);}).join('');
 	sendKey(combined);
 }
 $(document).ready(function() {
@@ -52,12 +52,9 @@ $(document).ready(function() {
 	const upCamTurnButton = document.getElementById('upCamTurn');
 	const downCamTurnButton = document.getElementById('downCamTurn');
 
-	// Create JoyStick object into the DIV 'joyDiv'
-	//const Joy = new JoyStick('joyDiv');
-
 	//Send camera instructions
 	function sendDirections(direction) {
-		$.get("cameraSaver.php", {direction:direction},function success(e){});
+		$.get("cameraSaver.php", {direction:direction});
 	}
 
 	//Camera buttons
@@ -78,36 +75,36 @@ $(document).ready(function() {
 
 	//Joystick
 	setInterval(function(){
-		//get joystick coordinates
-		var x = getPos().x;//Joy.GetX()/100;
-		var y = getPos().y;//Joy.GetY()/100;
-		var minSpeed = document.getElementById('slide').value;
+	    var VL, VR;
+        //get joystick coordinates
+    	var x = getPos().x;
+    	var y = getPos().y;
+    	var minSpeed = document.getElementById('slide').value;
 
-		//convert to polar
-		var r = Math.hypot(x, y);
-		if (r>1) {r=1}//because it's a square joystick
-		var t = Math.atan2(y, x);
+    	//Checker whether we should use car or tank drive
+	    if(document.getElementById('tdm').checked) {
+	        //Tank drive mode radio button is checked
 
-		//rotate by 45 degrees
-		t -= Math.PI / 4;
+    		//convert to polar
+    		var r = Math.hypot(x, y);
+    		if (r>1) r=1; // because it's a square joystick
+    		var t = Math.atan2(y, x) - Math.PI / 4; // theta with 45 degree rotation
 
-		//back to cartesian
-		var VL = r * Math.cos(t);
-		var VR = r * Math.sin(t);
+    		//back to cartesian, rescale the new coords and clamp to -1/+1 with motor correction
+    		VL = Math.max(-1, Math.min((r * Math.cos(t)) * Math.sqrt(2), 1))*1000;
+    		VR = Math.max(-1, Math.min((r * Math.sin(t)) * Math.sqrt(2), 1))*1000;
 
-		//rescale the new coords
-		VL = VL * Math.sqrt(2);
-		VR = VR * Math.sqrt(2);
-
-		//clamp to -1/+1
-		VL = Math.max(-1, Math.min(VL, 1))*1000;
-		VR = Math.max(-1, Math.min(VR, 1))*1000;
+        }else if(document.getElementById('cdm').checked) {
+            //Car drive mode radio button is checked
+            VL = 1000*y; //Thrust motor
+            VR = 1000*x; //Steer motor
+        }else alert("Please select one of the options.");
 
 		//Apply minimum at which motors start moving
 		//VL
-		if (VL != 0) VL = Math.round(VL/Math.abs(VL)*minSpeed + VL/1000 *(1000 - minSpeed));
+		if (VL !== 0) VL = Math.round(VL/Math.abs(VL)*minSpeed + VL/1000 *(1000 - minSpeed));
 		//VR
-		if (VR != 0) VR = Math.round(VR/Math.abs(VR)*minSpeed + VR/1000 *(1000 - minSpeed));
+		if (VR !== 0) VR = Math.round(VR/Math.abs(VR)*minSpeed + VR/1000 *(1000 - minSpeed));
 
 		//Convert to arduino readable values
 		//lSend
@@ -129,6 +126,6 @@ $(document).ready(function() {
 	//Handle mouse releases
 	document.addEventListener('mouseup', () => {
 		sendDirections(' ');
-		if (keys.length == 0) sendKey('');
+		if (keys.length === 0) sendKey('');
 	});
 });
