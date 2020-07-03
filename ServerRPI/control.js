be9e38e30f77dd66c7bd17084d2518ce05d5118e
Kasper van Maasdam <kaspervanm@gmail.com>
2020-07-03 12:05:29 +0200

    Update control.js

--- ServerRPI/control.js
+++ ServerRPI/control.js
@@ -1,153 +1,134 @@
-// Declare some vars
+//Declare some vars
 var keys = [];
 var KeyboardHelper = { 65: "600l-", 81: "600l+", 68: "600r-", 69: "600r+" };
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
 
-// Update slider amount(and add it to KeyboardHelper var)
+//Update slider amount(and add it to KeyboardHelper var)
 function updateSlider(slideAmount) {
-  var sliderDiv = document.getElementById("sliderAmount");
-  sliderDiv.innerHTML = slideAmount;
-  var n = slideAmount.toString();
-  KeyboardHelper = { 65: n + "l-", 81: n + "l+", 68: n + "r-", 69: n + "r+" };
+	var sliderDiv = document.getElementById("sliderAmount");
+	sliderDiv.innerHTML = slideAmount;
+	var n = slideAmount.toString();
+	KeyboardHelper = { 65: n+"l-", 81: n+"l+", 68: n+"r-", 69: n+"r+" };
 }
 
-// Handle key presses
+//Handle key presses
 function keyDownHandler(event) {
-  var pressed = event.keyCode;
-  if (pressed in KeyboardHelper && !keys.includes(pressed)) {
-    var l = 0;
-    var r = 0;
-    keys.map(function (num) {
-      if (KeyboardHelper[num].includes("l")) {
-        l++;
-      } else {
-        r++;
-      }
-    });
-    if (
-      (KeyboardHelper[pressed].includes("l") && l === 0) ||
-      (KeyboardHelper[pressed].includes("r") && r === 0)
-    ) {
-      keys.push(pressed);
-      combine();
-    }
-  }
+	pressed = event.keyCode;
+	if (pressed in KeyboardHelper && !keys.includes(pressed)) {
+		l = 0;
+		r = 0;
+		keys.map(function (num){if (KeyboardHelper[num].includes("l")) {l++;} else {r++;}});
+		if ((KeyboardHelper[pressed].includes("l") && l == 0) || (KeyboardHelper[pressed].includes("r") && r == 0)){
+			keys.push(pressed);
+			combine();
+		}
+	}
 }
 
-// Handle key releases
+//Handle key releases
 function keyUpHandler(event) {
-  var index = keys.indexOf(event.keyCode);
-  if (index > -1) {
-    keys.splice(index, 1);
-    combine();
-  }
+	index = keys.indexOf(event.keyCode);
+	if (index > -1) {
+		keys.splice(index, 1);
+		combine();
+	}
 }
 
-// Send motor instructions
+//Send motor instructions
 function sendKey(key) {
-  console.log(key);
-  $.get("saver.php", { key: key });
+	console.log(key);
+	$.get("saver.php", {key:key});//,function success(e){/*$(".controls").append(key).append("  -  ");*/});
 }
-// Send keys that where used
+//Send keys that where used
 function combine() {
-  var combined = keys
-    .map(function (num) {
-      if (num in KeyboardHelper) return KeyboardHelper[num];
-    })
-    .join("");
-  sendKey(combined);
+	combined = keys.map(function (num){if (num in KeyboardHelper) return (KeyboardHelper[num]);}).join('');
+	sendKey(combined);
 }
-$(document).ready(function () {
-  // Camera button elements
-  const leftCamTurnButton = document.getElementById("leftCamTurn");
-  const rightCamTurnButton = document.getElementById("rightCamTurn");
-  const upCamTurnButton = document.getElementById("upCamTurn");
-  const downCamTurnButton = document.getElementById("downCamTurn");
+$(document).ready(function() {
+	//Camera button elements
+	const leftCamTurnButton = document.getElementById('leftCamTurn');
+	const rightCamTurnButton = document.getElementById('rightCamTurn');
+	const upCamTurnButton = document.getElementById('upCamTurn');
+	const downCamTurnButton = document.getElementById('downCamTurn');
 
-  // Create JoyStick object into the DIV 'joyDiv'
-  const Joy = new JoyStick("joyDiv");
+	// Create JoyStick object into the DIV 'joyDiv'
+	//const Joy = new JoyStick('joyDiv');
 
-  // Send camera instructions
-  function sendDirections(direction) {
-    $.get("cameraSaver.php", { direction: direction });
-  }
+	//Send camera instructions
+	function sendDirections(direction) {
+		$.get("cameraSaver.php", {direction:direction},function success(e){});
+	}
 
-  // Camera buttons
-  leftCamTurnButton.addEventListener("mousedown", () => {
-    sendDirections("h+");
-  });
-  rightCamTurnButton.addEventListener("mousedown", () => {
-    sendDirections("h-");
-  });
-  upCamTurnButton.addEventListener("mousedown", () => {
-    sendDirections("v-");
-  });
-  downCamTurnButton.addEventListener("mousedown", () => {
-    sendDirections("v+");
-  });
+	//Camera buttons
+	leftCamTurnButton.addEventListener('mousedown', () => {
+		sendDirections('h+');
+	});
+	rightCamTurnButton.addEventListener('mousedown', () => {
+		sendDirections('h-');
+	});
+	upCamTurnButton.addEventListener('mousedown', () => {
+		sendDirections('v-');
+	});
+	downCamTurnButton.addEventListener('mousedown', () => {
+		sendDirections('v+');
+	});
 
-  var prevSpeed = " ";
+	var prevSpeed = " ";
 
-  // Joystick
-  setInterval(function () {
-    var VL, VR;
-    // get joystick coordinates
-    var x = Joy.GetX() / 100;
-    var y = Joy.GetY() / 100;
-    var minSpeed = document.getElementById("slide").value;
+	//Joystick
+	setInterval(function(){
+		//get joystick coordinates
+		var x = getPos().x;//Joy.GetX()/100;
+		var y = getPos().y;//Joy.GetY()/100;
+		var minSpeed = document.getElementById('slide').value;
 
-    // Checker whether we should use car or tank drive
-    if (document.getElementById("tdm").checked) {
-      // Tank drive mode radio button is checked
+		//convert to polar
+		var r = Math.hypot(x, y);
+		if (r>1) {r=1}//because it's a square joystick
+		var t = Math.atan2(y, x);
 
-      // convert to polar
-      var r = Math.hypot(x, y);
-      if (r > 1) r = 1; // because it's a square joystick
-      var t = Math.atan2(y, x) - Math.PI / 4; // theta with 45 degree rotation
+		//rotate by 45 degrees
+		t -= Math.PI / 4;
 
-      // back to cartesian, rescale the new coords and clamp to -1/+1 with motor
-      // correction
-      VL = Math.max(-1, Math.min(r * Math.cos(t) * Math.sqrt(2), 1)) * 1000;
-      VR = Math.max(-1, Math.min(r * Math.sin(t) * Math.sqrt(2), 1)) * 1000;
-    } else if (document.getElementById("cdm").checked) {
-      // Car drive mode radio button is checked
-      VL = 1000 * y; // Thrust motor
-      VR = 1000 * x; // Steer motor
-    } else alert("Please select one of the options.");
+		//back to cartesian
+		var VL = r * Math.cos(t);
+		var VR = r * Math.sin(t);
 
-    // Apply minimum at which motors start moving
-    // VL
-    if (VL !== 0)
-      VL = Math.round(
-        (VL / Math.abs(VL)) * minSpeed + (VL / 1000) * (1000 - minSpeed)
-      );
-    // VR
-    if (VR !== 0)
-      VR = Math.round(
-        (VR / Math.abs(VR)) * minSpeed + (VR / 1000) * (1000 - minSpeed)
-      );
+		//rescale the new coords
+		VL = VL * Math.sqrt(2);
+		VR = VR * Math.sqrt(2);
 
-    // Convert to arduino readable values
-    // lSend
-    var lSend = Math.abs(VL).toString().concat("l");
-    if (VL < 0) lSend = lSend.concat("-");
-    else lSend = lSend.concat("+");
-    // rSend
-    var rSend = Math.abs(VR).toString().concat("r");
-    if (VR < 0) rSend = rSend.concat("-");
-    else rSend = rSend.concat("+");
+		//clamp to -1/+1
+		VL = Math.max(-1, Math.min(VL, 1))*1000;
+		VR = Math.max(-1, Math.min(VR, 1))*1000;
 
-    var msg = lSend.concat(rSend);
-    if (msg !== prevSpeed) {
-      prevSpeed = msg;
-      sendKey(msg);
-    }
-  }, 25);
+		//Apply minimum at which motors start moving
+		//VL
+		if (VL != 0) VL = Math.round(VL/Math.abs(VL)*minSpeed + VL/1000 *(1000 - minSpeed));
+		//VR
+		if (VR != 0) VR = Math.round(VR/Math.abs(VR)*minSpeed + VR/1000 *(1000 - minSpeed));
 
-  // Handle mouse releases
-  document.addEventListener("mouseup", () => {
-    sendDirections(" ");
-    if (keys.length === 0) sendKey("");
-  });
+		//Convert to arduino readable values
+		//lSend
+		var lSend = Math.abs(VL).toString().concat("l");
+		if (VL<0) lSend = lSend.concat("-");
+		else lSend = lSend.concat("+");
+		//rSend
+		var rSend = Math.abs(VR).toString().concat("r");
+		if (VR<0)  rSend = rSend.concat("-");
+		else rSend = rSend.concat("+");
+
+		var msg = lSend.concat(rSend);
+		if (msg !== prevSpeed){
+			prevSpeed = msg;
+			sendKey(msg);
+		}
+	}, 25);
+
+	//Handle mouse releases
+	document.addEventListener('mouseup', () => {
+		sendDirections(' ');
+		if (keys.length == 0) sendKey('');
+	});
 });
