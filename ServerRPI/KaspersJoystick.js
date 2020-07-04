635f68242eaa8d8b0d229e2772ac50d6d4ec6d41
Restyled.io <commits@restyled.io>
2020-07-04 09:53:09 +0000

    Restyled by clang-format

--- ServerRPI/KaspersJoystick.js
+++ ServerRPI/KaspersJoystick.js
@@ -1,65 +1,88 @@
 $(document).ready(setTimeout(function() {
-	$("#joystick_box").append("<div id='joystick'></div>");
-	$("#joystick").width("33%").height("33%");//33*$("#joystick_box").height()/100);//33*$("#joystick_box").width()/100);
-	$("#joystick").css("background-color", "black").css("position", "relative").css("border-radius", "50%");
-	$("#joystick_box").css("background-color", "white");
-	updateJoystickPos();
+  $("#joystick_box").append("<div id='joystick'></div>");
+  $("#joystick")
+      .width("33%")
+      .height(
+          "33%"); // 33*$("#joystick_box").height()/100);//33*$("#joystick_box").width()/100);
+  $("#joystick")
+      .css("background-color", "black")
+      .css("position", "relative")
+      .css("border-radius", "50%");
+  $("#joystick_box").css("background-color", "white");
+  updateJoystickPos();
 
-	// Make the DIV element draggable:
-	var radius = $("#joystick_box").width()/2;
-	var zoom = $('#joystick').css('zoom');
-	$('#joystick').draggable({
-		drag: function( event, ui ) {
-			var x = ui.position.left+$("#joystick").width()/2 - radius,
-			y = radius - (ui.position.top+$("#joystick").height()/2),
-			h = Math.hypot(x, y);
+  // Make the DIV element draggable:
+  var radius = $("#joystick_box").width() / 2;
+  var zoom = $('#joystick').css('zoom');
+  $('#joystick').draggable({
+    drag : function(event, ui) {
+      var x = ui.position.left + $("#joystick").width() / 2 - radius,
+          y = radius - (ui.position.top + $("#joystick").height() / 2),
+          h = Math.hypot(x, y);
 
-			var factor = (1 / zoom) -1;
-			ui.position.top += Math.round((ui.position.top - ui.originalPosition.top) * factor);
-			ui.position.left += Math.round((ui.position.left- ui.originalPosition.left) * factor);
+      var factor = (1 / zoom) - 1;
+      ui.position.top +=
+          Math.round((ui.position.top - ui.originalPosition.top) * factor);
+      ui.position.left +=
+          Math.round((ui.position.left - ui.originalPosition.left) * factor);
 
-			if (Math.floor(h) > radius) {
-				ui.position.top =  radius - Math.round(radius * y / h) - $("#joystick").height()/2;
-				ui.position.left = Math.round(radius * x / h) + radius - $("#joystick").width()/2;
-			}
-		},
-		revert: true,
-		revertDuration: 300,
-		scroll: false
-	});
+      if (Math.floor(h) > radius) {
+        ui.position.top =
+            radius - Math.round(radius * y / h) - $("#joystick").height() / 2;
+        ui.position.left =
+            Math.round(radius * x / h) + radius - $("#joystick").width() / 2;
+      }
+    },
+    revert : true,
+    revertDuration : 300,
+    scroll : false
+  });
 }, '2000'));
 
 function updateJoystickPos() {
-	$("#joystick").offset({top: Math.round(getPos(1).y - $("#joystick").height()/2), left: Math.round(getPos(1).x - $("#joystick").width()/2)});
+  $("#joystick").offset({
+    top : Math.round(getPos(1).y - $("#joystick").height() / 2),
+    left : Math.round(getPos(1).x - $("#joystick").width() / 2)
+  });
 }
 
-function getPos(parentonly = -1){
-	var joystick_box_pos_middle = {};
-	joystick_box_pos_middle.y = $("#joystick_box").offset().top + $("#joystick_box").height()/2;
-	joystick_box_pos_middle.x = $("#joystick_box").offset().left + $("#joystick_box").width()/2;
+function getPos(parentonly = -1) {
+  var joystick_box_pos_middle = {};
+  joystick_box_pos_middle.y =
+      $("#joystick_box").offset().top + $("#joystick_box").height() / 2;
+  joystick_box_pos_middle.x =
+      $("#joystick_box").offset().left + $("#joystick_box").width() / 2;
 
-	if(parentonly == 1) return joystick_box_pos_middle;
+  if (parentonly == 1)
+    return joystick_box_pos_middle;
 
   var joystick_pos_middle = {};
-	joystick_pos_middle.y = $("#joystick").offset().top + $("#joystick").height()/2;
-	joystick_pos_middle.x = $("#joystick").offset().left + $("#joystick").width()/2;
+  joystick_pos_middle.y =
+      $("#joystick").offset().top + $("#joystick").height() / 2;
+  joystick_pos_middle.x =
+      $("#joystick").offset().left + $("#joystick").width() / 2;
 
-	var joystick_pos_middle_relative = relativate(joystick_box_pos_middle, joystick_pos_middle);
+  var joystick_pos_middle_relative =
+      relativate(joystick_box_pos_middle, joystick_pos_middle);
 
-	if(parentonly == 100){
-		var ret = {};
-		ret.x = parseInt(joystick_pos_middle_relative.x/($("#joystick_box").width()/2) * 100) / 100;
-		ret.y = parseInt(joystick_pos_middle_relative.y/($("#joystick_box").height()/2) * 100) / 100;
-		return ret;
-	}
+  if (parentonly == 100) {
+    var ret = {};
+    ret.x = parseInt(joystick_pos_middle_relative.x /
+                     ($("#joystick_box").width() / 2) * 100) /
+            100;
+    ret.y = parseInt(joystick_pos_middle_relative.y /
+                     ($("#joystick_box").height() / 2) * 100) /
+            100;
+    return ret;
+  }
 
-	return joystick_pos_middle_relative;
+  return joystick_pos_middle_relative;
 }
 
-function relativate(parentPos, childPos){
-	var relativePos = {};
-	relativePos.x = childPos.x - parentPos.x;
-	relativePos.y = parentPos.y - childPos.y;
-	return relativePos;
+function relativate(parentPos, childPos) {
+  var relativePos = {};
+  relativePos.x = childPos.x - parentPos.x;
+  relativePos.y = parentPos.y - childPos.y;
+  return relativePos;
 }
 window.addEventListener('resize', updateJoystickPos);
--- ServerRPI/control.js
+++ ServerRPI/control.js
@@ -1,135 +1,154 @@
-//Declare some vars
+// Declare some vars
 var keys = [];
-var KeyboardHelper = { 65: "600l-", 81: "600l+", 68: "600r-", 69: "600r+" };
+var KeyboardHelper = {65 : "600l-", 81 : "600l+", 68 : "600r-", 69 : "600r+"};
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
 
-//Update slider amount(and add it to KeyboardHelper var)
+// Update slider amount(and add it to KeyboardHelper var)
 function updateSlider(slideAmount) {
-	var sliderDiv = document.getElementById("sliderAmount");
-	sliderDiv.innerHTML = slideAmount;
-	var n = slideAmount.toString();
-	KeyboardHelper = { 65: n+"l-", 81: n+"l+", 68: n+"r-", 69: n+"r+" };
+  var sliderDiv = document.getElementById("sliderAmount");
+  sliderDiv.innerHTML = slideAmount;
+  var n = slideAmount.toString();
+  KeyboardHelper = {65 : n + "l-", 81 : n + "l+", 68 : n + "r-", 69 : n + "r+"};
 }
 
-//Handle key presses
+// Handle key presses
 function keyDownHandler(event) {
-	var pressed = event.keyCode;
-	if (pressed in KeyboardHelper && !keys.includes(pressed)) {
-		var l = 0;
-		var r = 0;
-		keys.map(function (num){if (KeyboardHelper[num].includes("l")) {l++;} else {r++;}});
-		if ((KeyboardHelper[pressed].includes("l") && l === 0) || (KeyboardHelper[pressed].includes("r") && r === 0)){
-			keys.push(pressed);
-			combine();
-		}
-	}
+  var pressed = event.keyCode;
+  if (pressed in KeyboardHelper && !keys.includes(pressed)) {
+    var l = 0;
+    var r = 0;
+    keys.map(function(num) {
+      if (KeyboardHelper[num].includes("l")) {
+        l++;
+      } else {
+        r++;
+      }
+    });
+    if ((KeyboardHelper[pressed].includes("l") && l === 0) ||
+        (KeyboardHelper[pressed].includes("r") && r === 0)) {
+      keys.push(pressed);
+      combine();
+    }
+  }
 }
 
-//Handle key releases
+// Handle key releases
 function keyUpHandler(event) {
-	var index = keys.indexOf(event.keyCode);
-	if (index > -1) {
-		keys.splice(index, 1);
-		combine();
-	}
+  var index = keys.indexOf(event.keyCode);
+  if (index > -1) {
+    keys.splice(index, 1);
+    combine();
+  }
 }
 
-//Send motor instructions
+// Send motor instructions
 function sendKey(key) {
-	console.log(key);
-	$.get("saver.php", {key:key});
+  console.log(key);
+  $.get("saver.php", {key : key});
 }
-//Send keys that where used
+// Send keys that where used
 function combine() {
-	var combined = keys.map(function (num){if (num in KeyboardHelper) return (KeyboardHelper[num]);}).join('');
-	sendKey(combined);
+  var combined = keys.map(function(num) {
+                       if (num in KeyboardHelper)
+                         return (KeyboardHelper[num]);
+                     })
+                     .join('');
+  sendKey(combined);
 }
 $(document).ready(function() {
-	//Camera button elements
-	const leftCamTurnButton = document.getElementById('leftCamTurn');
-	const rightCamTurnButton = document.getElementById('rightCamTurn');
-	const upCamTurnButton = document.getElementById('upCamTurn');
-	const downCamTurnButton = document.getElementById('downCamTurn');
+  // Camera button elements
+  const leftCamTurnButton = document.getElementById('leftCamTurn');
+  const rightCamTurnButton = document.getElementById('rightCamTurn');
+  const upCamTurnButton = document.getElementById('upCamTurn');
+  const downCamTurnButton = document.getElementById('downCamTurn');
 
-	//Send camera instructions
-	function sendDirections(direction) {
-		$.get("cameraSaver.php", {direction:direction});
-	}
+  // Send camera instructions
+  function sendDirections(direction) {
+    $.get("cameraSaver.php", {direction : direction});
+  }
 
-	//Camera buttons
-	leftCamTurnButton.addEventListener('mousedown', () => {
-		sendDirections('h+');
-	});
-	rightCamTurnButton.addEventListener('mousedown', () => {
-		sendDirections('h-');
-	});
-	upCamTurnButton.addEventListener('mousedown', () => {
-		sendDirections('v-');
-	});
-	downCamTurnButton.addEventListener('mousedown', () => {
-		sendDirections('v+');
-	});
+  // Camera buttons
+  leftCamTurnButton.addEventListener('mousedown',
+                                     () => { sendDirections('h+'); });
+  rightCamTurnButton.addEventListener('mousedown',
+                                      () => { sendDirections('h-'); });
+  upCamTurnButton.addEventListener('mousedown',
+                                   () => { sendDirections('v-'); });
+  downCamTurnButton.addEventListener('mousedown',
+                                     () => { sendDirections('v+'); });
 
-	var prevSpeed = " ";
+  var prevSpeed = " ";
 
-	//Joystick
-	setInterval(function(){
-	    var VL, VR;
-        //get joystick coordinates
-    	var x = getPos(100).x;
-    	var y = getPos(100).y;
-    	var minSpeed = document.getElementById('slide').value;
+  // Joystick
+  setInterval(function() {
+    var VL, VR;
+    // get joystick coordinates
+    var x = getPos(100).x;
+    var y = getPos(100).y;
+    var minSpeed = document.getElementById('slide').value;
 
-    	//Checker whether we should use car or tank drive
-	    if(document.getElementById('tdm').checked) {
-	        //Tank drive mode radio button is checked
+    // Checker whether we should use car or tank drive
+    if (document.getElementById('tdm').checked) {
+      // Tank drive mode radio button is checked
 
-    		//convert to polar
-    		var r = Math.hypot(x, y);
-    		if (r>1) r=1; // because it's a square joystick
-    		var t = Math.atan2(y, x) - Math.PI / 4; // theta with 45 degree rotation
+      // convert to polar
+      var r = Math.hypot(x, y);
+      if (r > 1)
+        r = 1;                                // because it's a square joystick
+      var t = Math.atan2(y, x) - Math.PI / 4; // theta with 45 degree rotation
 
-    		//back to cartesian, rescale the new coords and clamp to -1/+1 with motor correction
-    		VL = Math.max(-1, Math.min((r * Math.cos(t)) * Math.sqrt(2), 1))*1000;
-    		VR = Math.max(-1, Math.min((r * Math.sin(t)) * Math.sqrt(2), 1))*1000;
+      // back to cartesian, rescale the new coords and clamp to -1/+1 with motor
+      // correction
+      VL = Math.max(-1, Math.min((r * Math.cos(t)) * Math.sqrt(2), 1)) * 1000;
+      VR = Math.max(-1, Math.min((r * Math.sin(t)) * Math.sqrt(2), 1)) * 1000;
 
-        }else if(document.getElementById('cdm').checked) {
-            //Car drive mode radio button is checked
-            VL = 1000*y; //Thrust motor
-            VR = 1000*x; //Steer motor
-        }else alert("Please select one of the options.");
+    } else if (document.getElementById('cdm').checked) {
+      // Car drive mode radio button is checked
+      VL = 1000 * y; // Thrust motor
+      VR = 1000 * x; // Steer motor
+    } else
+      alert("Please select one of the options.");
 
-		//Apply minimum at which motors start moving
-		//VL
-		if (VL !== 0) VL = Math.round(VL/Math.abs(VL)*minSpeed + VL/1000 *(1000 - minSpeed));
-		//VR
-		if (VR !== 0) VR = Math.round(VR/Math.abs(VR)*minSpeed + VR/1000 *(1000 - minSpeed));
+    // Apply minimum at which motors start moving
+    // VL
+    if (VL !== 0)
+      VL = Math.round(VL / Math.abs(VL) * minSpeed +
+                      VL / 1000 * (1000 - minSpeed));
+    // VR
+    if (VR !== 0)
+      VR = Math.round(VR / Math.abs(VR) * minSpeed +
+                      VR / 1000 * (1000 - minSpeed));
 
-		//Convert to arduino readable values
-		//lSend
-		var lSend = Math.abs(VL).toString().concat("l");
-		if (VL<0) lSend = lSend.concat("-");
-		else lSend = lSend.concat("+");
-		//rSend
-		var rSend = Math.abs(VR).toString().concat("r");
-		if (VR<0)  rSend = rSend.concat("-");
-		else rSend = rSend.concat("+");
+    // Convert to arduino readable values
+    // lSend
+    var lSend = Math.abs(VL).toString().concat("l");
+    if (VL < 0)
+      lSend = lSend.concat("-");
+    else
+      lSend = lSend.concat("+");
+    // rSend
+    var rSend = Math.abs(VR).toString().concat("r");
+    if (VR < 0)
+      rSend = rSend.concat("-");
+    else
+      rSend = rSend.concat("+");
 
-		var msg = lSend.concat(rSend);
-		if (msg !== prevSpeed){
-			prevSpeed = msg;
-			if (VL == 0 && VR == 0) {
-				sendKey('');
-				return;
-			}
-			sendKey(msg);
-		}
-	}, 300);
+    var msg = lSend.concat(rSend);
+    if (msg !== prevSpeed) {
+      prevSpeed = msg;
+      if (VL == 0 && VR == 0) {
+        sendKey('');
+        return;
+      }
+      sendKey(msg);
+    }
+  }, 300);
 
-	//Handle mouse releases
-	document.addEventListener('mouseup', () => {
-		sendDirections(' ');
-		if (keys.length === 0) sendKey('');
-	});
+  // Handle mouse releases
+  document.addEventListener('mouseup', () => {
+    sendDirections(' ');
+    if (keys.length === 0)
+      sendKey('');
+  });
 });
