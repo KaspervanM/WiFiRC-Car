d5aa2007b20bdf62ecf6e85bcf2e605f61863898
Evan Pacini <65279226+evanpacini@users.noreply.github.com>
2020-07-10 10:55:41 +0200

    Rectification due to dumb robot editing my code.

--- ServerRPI/control.js
+++ ServerRPI/control.js
@@ -1,12 +1,17 @@
 // Declare some vars
 var keys = [];
-var KeyboardHelperTank = { 65: "600l-", 81: "600l+", 68: "600r-", 69: "600r+" };
+var KeyboardHelperTank = { 
+  65: "600l-", 
+  81: "600l+", 
+  68: "600r-", 
+  69: "600r+"
+  };
 var KeyboardHelperCar = {
   83: "600l-",
   87: "600l+",
   65: "600r-",
   68: "600r+",
-}; // Right now I'm using the left motor as if it was thrust.
+  }; // Right now I'm using the left motor as if it was thrust.
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
@@ -32,45 +37,17 @@ function updateSlider(slideAmount) {
 // Handle key presses
 function keyDownHandler(event) {
   var pressed = event.keyCode;
-  if (
-    pressed in KeyboardHelperTank &&
-    !keys.includes(pressed) &&
-    document.getElementById("tdm").checked
-  ) {
-    var l = 0,
-      r = 0;
-    keys.map(function (num) {
-      if (KeyboardHelperTank[num].includes("l")) {
-        l++;
-      } else {
-        r++;
-      }
-    });
-    if (
-      (KeyboardHelperTank[pressed].includes("l") && l === 0) ||
-      (KeyboardHelperTank[pressed].includes("r") && r === 0)
-    ) {
+  if (pressed in KeyboardHelperTank && keys.includes(pressed) && document.getElementById("tdm").checked) {
+    var l = 0, r = 0;
+    keys.map(function (num) { if (KeyboardHelperTank[num].includes("l")) {l++;} else {r++;} });
+    if ((KeyboardHelperTank[pressed].includes("l") && l === 0) || (KeyboardHelperTank[pressed].includes("r") && r === 0)) {
       keys.push(pressed);
       combine();
     }
-  } else if (
-    pressed in KeyboardHelperCar &&
-    !keys.includes(pressed) &&
-    document.getElementById("cdm").checked
-  ) {
-    var l = 0,
-      r = 0;
-    keys.map(function (num) {
-      if (KeyboardHelperCar[num].includes("l")) {
-        l++;
-      } else {
-        r++;
-      }
-    });
-    if (
-      (KeyboardHelperCar[pressed].includes("l") && l === 0) ||
-      (KeyboardHelperCar[pressed].includes("r") && r === 0)
-    ) {
+  } else if (pressed in KeyboardHelperCar && !keys.includes(pressed) && document.getElementById("cdm").checked) {
+    var l = 0, r = 0;
+    keys.map(function (num) { if (KeyboardHelperCar[num].includes("l")) {l++;} else {r++;} });
+    if ((KeyboardHelperCar[pressed].includes("l") && l === 0) || (KeyboardHelperCar[pressed].includes("r") && r === 0)) {
       keys.push(pressed);
       combine();
     }
@@ -96,18 +73,10 @@ function combine() {
   // Check whether we should use car or tank drive
   if (document.getElementById("tdm").checked) {
     // Tank drive mode radio button is checked
-    var combined = keys
-      .map(function (num) {
-        if (num in KeyboardHelperTank) return KeyboardHelperTank[num];
-      })
-      .join("");
+    var combined = keys.map(function (num) {if (num in KeyboardHelperTank) return KeyboardHelperTank[num];}).join("");
   } else if (document.getElementById("cdm").checked) {
     // Car drive mode radio button is checked
-    var combined = keys
-      .map(function (num) {
-        if (num in KeyboardHelperCar) return KeyboardHelperCar[num];
-      })
-      .join("");
+    var combined = keys.map(function (num) {if (num in KeyboardHelperCar) return KeyboardHelperCar[num];}).join("");
   }
   sendKey(combined);
 }
@@ -153,11 +122,9 @@ $(document).ready(function () {
 
       // convert to polar
       var r = Math.hypot(x, y);
-      if (r > 1) r = 1; // because it's a square joystick
       var t = Math.atan2(y, x) - Math.PI / 4; // theta with 45 degree rotation
 
-      // back to cartesian, rescale the new coords and clamp to -1/+1 with motor
-      // correction
+      // algorithm to go from polar VL and VR
       VL = Math.max(-1, Math.min(r * Math.cos(t) * Math.sqrt(2), 1)) * 1000;
       VR = Math.max(-1, Math.min(r * Math.sin(t) * Math.sqrt(2), 1)) * 1000;
     } else if (document.getElementById("cdm").checked) {
@@ -169,24 +136,20 @@ $(document).ready(function () {
     // Apply minimum at which motors start moving
     // VL
     if (VL !== 0)
-      VL = Math.round(
-        (VL / Math.abs(VL)) * minSpeed + (VL / 1000) * (1000 - minSpeed)
-      );
+      VL = Math.round((VL / Math.abs(VL)) * minSpeed + (VL / 1000) * (1000 - minSpeed));
     // VR
     if (VR !== 0)
-      VR = Math.round(
-        (VR / Math.abs(VR)) * minSpeed + (VR / 1000) * (1000 - minSpeed)
-      );
+      VR = Math.round((VR / Math.abs(VR)) * minSpeed + (VR / 1000) * (1000 - minSpeed));
 
     // Convert to arduino readable values
     // lSend
     var lSend = Math.abs(VL).toString().concat("l");
-    if (VL < 0) lSend = lSend.concat("-");
-    else lSend = lSend.concat("+");
+    if (VL < 0) { lSend = lSend.concat("-");
+    } else lSend = lSend.concat("+");
     // rSend
     var rSend = Math.abs(VR).toString().concat("r");
-    if (VR < 0) rSend = rSend.concat("-");
-    else rSend = rSend.concat("+");
+    if (VR < 0) { rSend = rSend.concat("-");
+    } else rSend = rSend.concat("+");
 
     var msg = lSend.concat(rSend);
     if (msg !== prevSpeed) {
